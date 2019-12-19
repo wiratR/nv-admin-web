734ce28924fd7c21fd8a306490bb9a65c02226e1
wiratR <wirateds@gmail.com>
2019-12-19 16:23:51 +0700

    done to create view
    done to create view

--- nv-app/src/components/Home/index.js
+++ nv-app/src/components/Home/index.js
@@ -1,38 +1,79 @@
-import React, { Component } from 'react';
-import { compose } from 'recompose';
+import React, { Component }  from 'react';
 
-import { withAuthorization, withEmailVerification } from '../Session';
-import { withFirebase } from '../Firebase';
-import Messages from '../Messages';
+import { withAuthorization } from '../Session';
+import firebase from 'firebase';
 
-class HomePage extends Component {
-  constructor(props) {
-    super(props);
+const HomePage = () => (
+  <div>
+    <h1>Home Page</h1>
+    < DeviceStatusForm/>
+  </div>
+);
 
-    this.state = {
-      users: null,
-    };
+// this class for get data from Realt time database
+class DeviceStatusForm extends Component  {
+  constructor(){
+      super();
+      this.state = {
+        dv_status:[],
+        item_id:'',
+        device_id:'',
+        datetime:'',
+        status:''
+      }
+      this.handleChange = this.handleChange.bind(this)
+
+  }
+  componentDidMount(){
+    const itemsRef = firebase.database().ref('dv_status');
+    itemsRef.on('value',(snapshot) => {
+        let dv_status = snapshot.val();
+        let newState = [];
+        for(let item in dv_status){
+          newState.push({
+              item_id:item,
+              device_id:dv_status[item].device_id,
+              datetime:dv_status[item].datetime,
+              status:dv_status[item].status
+          })
+        }
+        this.setState({
+          dv_status:newState
+        })
+    })
   }
 
-  componentDidMount() {
-    this.props.firebase.users().on('value', snapshot => {
-      this.setState({
-        users: snapshot.val(),
-      });
-    });
+  handleChange(e){
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
-
-  componentWillUnmount() {
-    this.props.firebase.users().off();
-  }
-
   render() {
     return (
-      <div>
-        <h1>Home Page</h1>
-        <p>The Home Page is accessible by every signed in user.</p>
-
-        <Messages users={this.state.users} />
+      <div className="app">
+          <nav class="navbar navbar-light bg-primary">
+            <span class="navbar-brand mb-0 h1">Device Status Details</span>
+          </nav>
+          <div className="container" style={{marginTop:70}}>
+              <table className="table table-sm table-bordered">
+                    <tr className="thead-dark">
+                      <th width="5%">Device</th>
+                      <th width="10%">Status</th>
+                      <th width="20%">Date time</th>
+                    </tr>
+                    {
+                        this.state.dv_status.map((item) => {
+                          return (
+                              <tr>
+                                <td>{item.device_id}</td>
+                                <td>{item.status}</td>
+                                <td>{item.datetime}</td>
+                               </tr>
+                          )
+                        })
+                    }
+                </table>
+        </div>
       </div>
     );
   }
@@ -40,8 +81,5 @@ class HomePage extends Component {
 
 const condition = authUser => !!authUser;
 
-export default compose(
-  withFirebase,
-  withEmailVerification,
-  withAuthorization(condition),
-)(HomePage);
+export default withAuthorization(condition)(HomePage);
+
